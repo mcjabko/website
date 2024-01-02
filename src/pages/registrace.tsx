6506bb5b30016bc08c2fb41c4aa7e79f6cf3dbbd
Petr Václavek <petr@vaclavek.cloud>
2024-01-02 20:08:39 +0100

    feat: register redirect to discord

--- src/pages/registrace.tsx
+++ src/pages/registrace.tsx
@@ -1,15 +1,16 @@
 import { PageLayout } from '@/layouts/PageLayout'
 import { type NextPage } from 'next'
-import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 const RegisterPage: NextPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('https://discord.mcjabko.cz')
+  }, [])
   return (
-    <PageLayout title='Registrace' description=''>
-      <div className='min-h-screen text-center'>
-        <h1 className='text-4xl font-bold'>Registrace jsou do až 2.1.2024  <strong>vypnuty</strong></h1>
-        <p className='text-xl mt-5'>Více informací nalezneš na našem discordu <strong><Link href='https://discord.mcjabko.cz'>zde</Link></strong></p>
-      </div>
+    <PageLayout title='' description=''>
+      <div>...</div>
     </PageLayout>
   )
 }
